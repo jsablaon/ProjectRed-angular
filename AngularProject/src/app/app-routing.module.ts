d0c77680eacff976675900ece3d4b5b0a199307c
Teresa <balbianit@gmail.com>
2022-10-17 12:31:59 -0700

    creating basic layout and navigation

--- AngularProject/src/app/app-routing.module.ts
+++ AngularProject/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountComponent } from './account/account.component';
+import { CartComponent } from './cart/cart.component';
+import { HomescreenComponent } from './homescreen/homescreen.component';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Account', component: AccountComponent},
+  {path: 'Cart', component: CartComponent},
+  {path: 'Home', component: HomescreenComponent},
+  {path: 'Login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ AngularProject/src/app/homescreen/homescreen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homescreen',
+  templateUrl: './homescreen.component.html',
+  styleUrls: ['./homescreen.component.css']
+})
+export class HomescreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
