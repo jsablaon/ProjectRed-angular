71d5b21e99a28dd96df354089d7dd11230473e2b
bcuser <bestauffer1@gmail.com>
2022-11-02 15:51:27 -0700

    Added mock items

---
+++ AngularProject/src/app/mock-items.ts
@@ -0,0 +1,9 @@
+import { Item } from './item'
+
+export const ITEMS: Item[] = [
+    { id: 1, name: 'banana', price: 5.95},
+    { id: 2, name: 'banana', price: 7.90},
+    { id: 3, name: 'banana', price: 8.05},
+    { id: 4, name: 'banana', price: 4.35},
+    { id: 5, name: 'banana', price: 2.00}
+];
